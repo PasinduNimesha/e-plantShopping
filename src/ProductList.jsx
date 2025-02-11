27874547757121f355db6267a972a76fe418f7fc
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2025-02-11 15:23:49 +0530

    Revert "Update ProductList.jsx"
    This reverts commit 6c49c0f3f93b1a32e7896aa88590f1c381650563.

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -212,7 +212,7 @@ function ProductList() {
             ]
         }
     ];
-    const styleObj={
+   const styleObj={
     backgroundColor: '#4CAF50',
     color: '#fff!important',
     padding: '15px',
@@ -221,22 +221,22 @@ function ProductList() {
     alignIems: 'center',
     fontSize: '20px',
    }
-    const styleObjUl={
+   const styleObjUl={
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
     width: '1100px',
    }
-    const styleA={
+   const styleA={
     color: 'white',
     fontSize: '30px',
     textDecoration: 'none',
    }
-    const handleCartClick = (e) => {
+   const handleCartClick = (e) => {
     e.preventDefault();
     setShowCart(true); // Set showCart to true when cart icon is clicked
 };
-    const handlePlantsClick = (e) => {
+const handlePlantsClick = (e) => {
     e.preventDefault();
     setShowPlants(true); // Set showAboutUs to true when "About Us" link is clicked
     setShowCart(false); // Hide the cart when navigating to About Us
